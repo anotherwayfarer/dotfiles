cc9cc4c84ebb7a7eaf9bc1c63e3c8318e530703d
Alexey Minchakov <lexaaim@gmail.com>
2016-12-04 13:53:41 +0300

    colorize free disk space

--- .config/i3/scripts/freespace.sh
+++ .config/i3/scripts/freespace.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
-    
-df -h "$1" | gawk 'NR == 2 {print $4}'
+
+VARSP="$(df -h "$1" | gawk 'NR == 2 {print $4}')"
+VARPERC="$(df -h "$1" | gawk 'NR == 2 {print $5}' | cut -f1 -d'%')"
+echo "$VARSP"
+echo "$VARSP"
+if [ $VARPERC -ge 70 ]; then
+    echo "#e74c3c"
+elif [ $VARPERC -ge 40 ]; then
+    echo "#f1c40f"
+fi
+
