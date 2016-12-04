c53de8055ef41dea631423df446af84ddbd5a36e
Alexey Minchakov <lexaaim@gmail.com>
2016-12-04 13:21:21 +0300

    colorize layout

--- .config/i3/scripts/kblayout.sh
+++ .config/i3/scripts/kblayout.sh
@@ -1,1 +1,10 @@
-~/DevTools/xkblayout-state print %s | tr [a-z] [A-Z]
+#!/bin/sh
+
+VARKL="$(~/DevTools/xkblayout-state print %s | tr [a-z] [A-Z])"
+echo $VARKL
+echo $VARKL
+case "$VARKL" in
+    "US" ) echo "#2ecc71";;
+    "RU" ) echo "#3498db";;
+esac
+
