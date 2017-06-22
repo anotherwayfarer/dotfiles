b444c3ccb3198b7e4391c77df940070fdaf163ce
Alexey Minchakov <lexaaim@gmail.com>
2017-06-23 00:59:21 +0300

    update backup scripts

--- Admin/yadisk_check.sh
+++ Admin/yadisk_check.sh
@@ -1,13 +1,16 @@
 #!/bin/bash -
 
-# define YAUSER, GPGPASS
+# export YAUSER, GPGPASS
 source yadisk_conf.sh
 
 # tar cvpzf $HOME/Admin/bkp/documents_$(date +%Y_%m_%d_%H_%M).tgz --exclude=/proc --exclude=/lost+found --exclude=/backup.tgz --exclude=/mnt --exclude=/sys $HOME/Documents
 
-DOCFILE=$HOME/Admin/bkp/documents_$(date +%Y_%m_%d_%H_%M).tgz
-tar cvpzf $DOCFILE $HOME/Documents
-gpg --batch --yes --passphrase $GPGPASS --cipher-algo AES256 -o $DOCFILE.gpg -c $DOCFILE
+DOCFILE=$(ls $HOME/Admin/bkp/*.tgz.gpg)
+DOCFILE=${DOCFILE:0:${#DOCFILE}-4}
+
+# tar cvpzf $DOCFILE $HOME/Documents
+gpg --batch --yes --passphrase $GPGPASS --cipher-algo AES256 -o $DOCFILE -d $DOCFILE.gpg
+tar xf $DOCFILE -C $HOME/Admin/bkp/
 
 # mkdir -p /mnt/yadisk
 # mount -t davfs -o user,uid=$USER,username=$YAUSER https://webdav.yandex.ru:443 /mnt/yadisk
